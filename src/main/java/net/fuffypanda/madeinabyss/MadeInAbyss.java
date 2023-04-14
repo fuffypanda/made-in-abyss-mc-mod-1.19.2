0e77886dcef9073a02ccc024d705f82d12b96607
fuffypanda <choirgeek2@gmail.com>
2023-04-14 13:32:01 -0500

    working dimension

--- src/main/java/net/fuffypanda/madeinabyss/MadeInAbyss.java
+++ src/main/java/net/fuffypanda/madeinabyss/MadeInAbyss.java
@@ -1,15 +1,21 @@
 package net.fuffypanda.madeinabyss;
 
 import net.fabricmc.api.ModInitializer;
+import net.fuffypanda.madeinabyss.item.ModItems;
+import net.fuffypanda.madeinabyss.worlds.dimension.ModDimensions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MadeInAbyss implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "madeinabyss";
+	public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
 
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+
+		ModDimensions.register();
+
 	}
 }
---
+++ src/main/java/net/fuffypanda/madeinabyss/worlds/dimension/ModDimensions.java
@@ -0,0 +1,21 @@
+package net.fuffypanda.madeinabyss.worlds.dimension;
+
+import net.fuffypanda.madeinabyss.MadeInAbyss;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+
+public class ModDimensions {
+
+
+    public static final RegistryKey<World> LAYER2_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
+            new Identifier(MadeInAbyss.MOD_ID, "layer2"));
+    public static final RegistryKey<DimensionType> LAYER2_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, LAYER2_DIMENSION_KEY.getValue());
+
+
+    public static void register() {
+        MadeInAbyss.LOGGER.debug("Registering ModDimensions for " + MadeInAbyss.MOD_ID);
+    }
+}
