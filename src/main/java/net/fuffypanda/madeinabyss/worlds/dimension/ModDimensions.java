7604674b5d20e176bb94fcd18ac8993f20ce9de2
fuffypanda <choirgeek2@gmail.com>
2023-04-15 03:15:22 -0500

    working layers 1 and 2, plus a test layer where im working on the noise generation and stuff

--- src/main/java/net/fuffypanda/madeinabyss/worlds/dimension/ModDimensions.java
+++ src/main/java/net/fuffypanda/madeinabyss/worlds/dimension/ModDimensions.java
@@ -9,11 +9,17 @@ import net.minecraft.world.dimension.DimensionType;
 
 public class ModDimensions {
 
+    public static final RegistryKey<World> LAYER1_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
+            new Identifier(MadeInAbyss.MOD_ID, "layer1"));
+    public static final RegistryKey<DimensionType> LAYER1_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, LAYER1_DIMENSION_KEY.getValue());
 
     public static final RegistryKey<World> LAYER2_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
             new Identifier(MadeInAbyss.MOD_ID, "layer2"));
     public static final RegistryKey<DimensionType> LAYER2_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, LAYER2_DIMENSION_KEY.getValue());
 
+    public static final RegistryKey<World> LAYERTEST_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
+            new Identifier(MadeInAbyss.MOD_ID, "layertest"));
+    public static final RegistryKey<DimensionType> LAYERTEST_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, LAYERTEST_DIMENSION_KEY.getValue());
 
     public static void register() {
         MadeInAbyss.LOGGER.debug("Registering ModDimensions for " + MadeInAbyss.MOD_ID);
